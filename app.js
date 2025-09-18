e5e79ea48edfaeccdf9587a0ff5d58c9eb07fd7d
sp4ce <javier.lujan.121@outlook.es>
2025-09-17 22:57:47 -0500

    feat: push names into array

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let names = [];
+
+function addName() {
+  let capturedName = document.getElementById('amigo').value;
+
+  if (!capturedName.trim()) {
+    alert('Por favor, inserte un nombre.');
+  }
+
+  if (capturedName) {
+    names.push(capturedName);
+    document.getElementById('amigo').value = '';
+  }
+}
+
+document.getElementById('addButton').addEventListener('click', addName);
